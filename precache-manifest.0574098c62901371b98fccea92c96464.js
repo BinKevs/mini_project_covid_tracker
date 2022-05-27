406d368d21358bbf7d43a2096ee83d1086c6ff8a
Kevin Bryan Buenaseda <kevinbryanbuenaseda@gmail.com>
2022-05-27 22:43:23 +0800

    Updates

---
+++ precache-manifest.0574098c62901371b98fccea92c96464.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76af6911e4a72eb0b86a366ad2781fba",
+    "url": "/mini_project_covid_tracker/index.html"
+  },
+  {
+    "revision": "d90b47a9a3921a30897c",
+    "url": "/mini_project_covid_tracker/static/css/main.509a62cd.chunk.css"
+  },
+  {
+    "revision": "09bc58dcf71ae8dafbb6",
+    "url": "/mini_project_covid_tracker/static/js/2.32d0b820.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/mini_project_covid_tracker/static/js/2.32d0b820.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d90b47a9a3921a30897c",
+    "url": "/mini_project_covid_tracker/static/js/main.2aebbec3.chunk.js"
+  },
+  {
+    "revision": "b6d3e6e8aba7fb1cec9b",
+    "url": "/mini_project_covid_tracker/static/js/runtime-main.4c03db72.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/mini_project_covid_tracker/static/media/image.d7265326.png"
+  }
+]);
